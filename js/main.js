a5e352c983e6c89ef4b07a84d4d076c0c713a125
tvanrys <tvanrys@fanshaweonline.ca>
2021-03-24 13:27:56 -0400

    finished refactoring

--- js/main.js
+++ js/main.js
@@ -2,17 +2,17 @@
   console.log('fired');
 
   // variables at the top
-  const sigils = document.querySelectorAll('.sigilContainer'),
+  const sigils = document.querySelector('#navList'),
         banner = document.querySelector('#houseImages');
 
-  // functions go here
   function animateBanner(event) {
-    let multiplier = this.dataset.offset,
-        offset = 600;
-    // 0, 600, 1200, 1800px depending on the math
-    banner.style.right = `${multiplier * offset}px`;
-    //debugger;
+    if (event.target.className.includes('sigilContainer')) {
+      let multiplier = event.target.dataset.offset,
+          offsetWidth = 600;
+      // 0, 600, 1200, 1800px depending on the math
+      banner.style.right = `${multiplier * offsetWidth}px`;
+    }
   }
 
-  sigils.forEach(sigil => sigil.addEventListener('click', animateBanner));
+  sigils.addEventListener('click', animateBanner);
 })();
